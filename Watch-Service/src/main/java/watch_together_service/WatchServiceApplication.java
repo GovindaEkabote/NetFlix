a0c95803f293fd4b89e482cd2994df281fb0bbf3
GovindaEkbote <govindaekbote7@gmail.com>
2026-07-19 00:26:07 +0530

    watch together service start

---
+++ Watch-Service/src/main/java/watch_together_service/WatchServiceApplication.java
@@ -0,0 +1,13 @@
+package watch_together_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchServiceApplication.class, args);
+	}
+
+}
